d69b37b67cb24a27909e48851ab2147eb066298a
Beko98 <99834191+Beko98@users.noreply.github.com>
2022-12-28 18:12:29 +0400

    final axios

--- main.js
+++ main.js
@@ -29,3 +29,30 @@ async function getInformation() {
 }
 
 getInformation();
+
+const button1 = document.getElementById("button1");
+const button2 = document.getElementById("button2");
+const info1 = document.getElementById("info1");
+const info2 = document.getElementById("info2");
+
+button1.addEventListener("click", async () => {
+  try {
+    const response = await axios.get("https://reqres.in/api/users?page=2");
+    info1.innerHTML = `total pages: ${response.data.total_pages}`;
+  } catch (error) {
+    console.error(error);
+    info1.innerHTML = "Error" + error.message;
+  }
+});
+
+button2.addEventListener("click", async () => {
+  try {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/todos/1"
+    );
+    info2.innerHTML = `title: ${response.data.title}`;
+  } catch {
+    console.error(error);
+    info2.innerHTML = "Error" + error.message;
+  }
+});
